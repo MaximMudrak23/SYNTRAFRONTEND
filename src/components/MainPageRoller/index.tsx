8188481482e43508903994cafac803ea33679931
Maxim <mudrak.maksym@chnu.edu.ua>
2025-07-14 18:00:25 +0300

    roller done + little fixes in slider

--- src/components/MainPageRoller/index.tsx
+++ src/components/MainPageRoller/index.tsx
@@ -1,7 +1,16 @@
 import zxc from './styles.module.scss'
+import { rollerObj } from '../../other/rollerData'
+const selectedLanguage = 'en';
 
 export function MainPageRoller() {
+    const rollerStr = rollerObj[`${selectedLanguage}`];
     return (
-        <div className={zxc.roller}></div>
+        <div className={zxc.roller}>
+            <div className={zxc.rollerWrapper}>
+                {[...rollerStr,...rollerStr].map((txt, index) => (
+                    <p key={index}>{txt}</p>
+                ))}
+            </div>
+        </div>
     )
 }
