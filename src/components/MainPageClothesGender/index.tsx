8541da7b2ba35c8dfe95089ac38f8cf6ff7b4038
Maxim <mudrak.maksym@chnu.edu.ua>
2025-07-15 10:57:00 +0300

    clothesgender done

--- src/components/MainPageClothesGender/index.tsx
+++ src/components/MainPageClothesGender/index.tsx
@@ -1,18 +1,11 @@
 import zxc from './styles.module.scss'
-
-const items = [
-    {img: '/i1.png', text: 'ФУТБОЛКИ'},
-    {img: '/i2.png', text: 'ДЖИНСИ'},
-    {img: '/i3.png', text: 'СОРОЧКИ'},
-    {img: '/i4.png', text: 'ШОРТИ'},
-    {img: '/i5.png', text: 'ТРИКОТАЖ'},
-    {img: '/i6.png', text: 'АКСЕСУАРИ'}
-]
+import { clothesManGenderData } from '../../other/clothesGenData';
+const selectedLanguage = 'ua';
 
 export function MainPageClothesGender() {
     return (
         <div className={zxc.clothesGender}>
-            {items.map((item, index) => (
+            {clothesManGenderData[`${selectedLanguage}`].map((item, index) => (
                 <div
                     key={index}
                     className={zxc.clothesCard}
---
+++ src/other/clothesGenData.ts
@@ -0,0 +1,26 @@
+export const clothesManGenderData = {
+    ua: [
+        {img: 'clothesGender/man/1.jpg', text: 'ФУТБОЛКИ'},
+        {img: 'clothesGender/man/2.jpg', text: 'ШОРТИ'},
+        {img: 'clothesGender/man/3.jpg', text: 'ДЖИНСИ'},
+        {img: 'clothesGender/man/4.jpg', text: 'СОРОЧКИ'},
+        {img: 'clothesGender/man/5.jpg', text: 'ШТАНИ'},
+        {img: 'clothesGender/man/6.jpg', text: 'АКСЕСУАРИ'}
+    ],
+    ru: [
+        {img: 'clothesGender/man/1.jpg', text: 'ФУТБОЛКИ'},
+        {img: 'clothesGender/man/2.jpg', text: 'ДЖИНСЫ'},
+        {img: 'clothesGender/man/3.jpg', text: 'РУБАШКИ'},
+        {img: 'clothesGender/man/4.jpg', text: 'ШОРТЫ'},
+        {img: 'clothesGender/man/5.jpg', text: 'ШТАНЫ'},
+        {img: 'clothesGender/man/6.jpg', text: 'АКСЕСУАРЫ'}
+    ],
+    en: [
+        {img: 'clothesGender/man/1.jpg', text: 'T-SHIRTS'},
+        {img: 'clothesGender/man/2.jpg', text: 'JEANS'},
+        {img: 'clothesGender/man/3.jpg', text: 'SHIRTS'},
+        {img: 'clothesGender/man/4.jpg', text: 'SHORTS'},
+        {img: 'clothesGender/man/5.jpg', text: 'TROUSERS'},
+        {img: 'clothesGender/man/6.jpg', text: 'ACCESSORIES'}
+    ],
+}
